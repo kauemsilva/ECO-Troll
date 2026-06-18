bd7b5eb831a59b9c83b23c80a068431b884252d1
Kauê Martins Silva <kauemsilva@gmail.com>
2026-06-17 23:47:15 -0300

    Fix: Linha 11 corrigida no script da armadilha

--- armadilha_espinho.gd
+++ armadilha_espinho.gd
@@ -3,12 +3,12 @@ extends Area2D
 var ativada = false
 
 func _ready():
-    body_entered.connect(_on_body_entered)
+	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-    if body.name == "Jogador" and not ativada:
-        ativada = true
-        .visible = true
-        await get_tree().create_timer(0.15).timeout
-        Global.perder_vida()
-        ativada = false
+	if body.name == "Jogador" and not ativada:
+		ativada = true
+		$Sprite2D.visible = true
+		await get_tree().create_timer(0.15).timeout
+		Global.perder_vida()
+		ativada = false
