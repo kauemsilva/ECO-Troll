ac9a6ad4e11b208e6c7107d22d1907e88cecde9c
Kauê Martins Silva <kauemsilva@gmail.com>
2026-06-18 00:11:03 -0300

    Fix: Indentacao do portal.gd corrigida via PS

--- portal.gd
+++ portal.gd
@@ -3,8 +3,8 @@ extends Area2D
 @export var proxima_fase: String = "res://fase_1.tscn"
 
 func _ready():
-body_entered.connect(_on_body_entered)
+	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-if body.name == "Jogador":
-Global.avancar_fase(proxima_fase)
+	if body.name == "Jogador":
+		Global.avancar_fase(proxima_fase)
