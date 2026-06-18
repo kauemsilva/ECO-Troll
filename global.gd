68a8fa1bc8cfaf6ed59074c95aa85b7d52c70377
Kauê Martins Silva <kauemsilva@gmail.com>
2026-06-18 00:02:07 -0300

    Fix: Indentacao do global.gd corrigida via PS

--- global.gd
+++ global.gd
@@ -5,22 +5,17 @@ var vidas_atuais: int = vidas_maximas
 var indestrutivel: bool = false
 
 func perder_vida():
-    if indestrutivel:
-        return
-    vidas_atuais -= 1
-    if vidas_atuais <= 0:
-        game_over()
-    else:
-        get_tree().reload_current_scene()
-
-func ativar_modo_turbo(duracao: float):
-    indestrutivel = true
-    await get_tree().create_timer(duracao).timeout
-    indestrutivel = false
+	if indestrutivel:
+		return
+	vidas_atuais -= 1
+	if vidas_atuais <= 0:
+		game_over()
+	else:
+		get_tree().reload_current_scene()
 
 func game_over():
-    vidas_atuais = vidas_maximas
-    get_tree().change_scene_to_file("res://fase_1.tscn")
+	vidas_atuais = vidas_maximas
+	get_tree().change_scene_to_file("res://game_over.tscn")
 
 func avancar_fase(proxima_fase: String):
-    get_tree().change_scene_to_file(proxima_fase)
+	get_tree().change_scene_to_file(proxima_fase)
