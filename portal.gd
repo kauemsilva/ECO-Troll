94ef4976487e8309330b9c4f1f5178c790a4e2a8
Kauê Martins Silva <kauemsilva@gmail.com>
2026-06-18 00:07:18 -0300

    Feature: Fluxo completo de jogo com 3 fases, itens e portais

---
+++ portal.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var proxima_fase: String = "res://fase_1.tscn"
+
+func _ready():
+body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body):
+if body.name == "Jogador":
+Global.avancar_fase(proxima_fase)
